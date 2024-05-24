6ed3b41ffa0ffd45ff22cfec0705bb145cfc338b
pigkiller <22373017@buaa.edu.cn>
2024-05-24 19:20:40 +0800

    0524 19:20 modify code

--- train_pre_handle.py
+++ train_pre_handle.py
@@ -4,13 +4,13 @@ from methods import get_job_description_summary
 
 # 读取CSV文件到DataFrame
 # train_data = pd.read_csv("train.csv", index_col=0)
-train_data = pd.read_csv("train.csv")
+train_data = pd.read_csv("new_train.csv")
 print(len(train_data))
 
 # 初始化职位描述摘要列
-train_data['job_description_summary'] = None
+# train_data['job_description_summary'] = None
 
-for i in range(len(train_data)):
+for i in range(10495, len(train_data)):
     train_data.loc[i, 'job_description_summary'] = (
         get_job_description_summary(train_data['job_title'][i], train_data['job_description'][i]))
 
