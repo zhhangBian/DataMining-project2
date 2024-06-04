77b19b67c78e09ab99d82b47f67703c88c6ad360
pigkiller <22373017@buaa.edu.cn>
2024-06-04 18:40:23 +0800

    run eval

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from eval import eval_job_classify
 from get_job_description_summary import get_job_description_summary
 from get_job_keywords import get_job_keywords
 from get_secondary_keywords import get_secondary_keywords
-from get_solution import get_solution
+from get_result import get_result
 from get_test_keywords import get_test_keywords
 
 
 def main():
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # 跑一次要很久，不要轻易运行!!!!!!!!
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # 获取工作的简化描述
     get_job_description_summary()
     # 获取职位和一二级职位的关键词
     get_job_keywords()
-    get_secondary_keywords()
+    # get_secondary_keywords()
     # 获取测试数据中的关键词
     get_test_keywords()
     # 生成结果
@@ -24,4 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    nltk.translate.bleu_score.sentence_bleu()
